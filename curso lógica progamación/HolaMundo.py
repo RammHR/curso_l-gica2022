f897c50ab11e45611dbf6528bcb066860dc21c3e
RammHR <116772563+RammHR@users.noreply.github.com>
2022-10-27 18:53:18 -0500

    1era subida de archivo de Python

---
+++ curso lógica progamación/HolaMundo.py
@@ -0,0 +1,9 @@
+#Ejemplo 1. Saludo
+#COMENTARIOS: Sirve para documentar o detallar cada parte del programa.
+#Los comenntarios los omite o no los toma en cuenta Python
+ 
+ #Imprimiendo o mostrando en pantalla mensaje o resultados
+print("Hola Mundo")
+print('Ramon');
+print(10+5)
+print(9*8)
