7520b8c8bab2c44150f86b5917d6cfdbe5626766
RammHR <116772563+RammHR@users.noreply.github.com>
2022-11-03 18:46:43 -0600

    ej2

---
+++ ej2.py
@@ -0,0 +1,18 @@
+#ENTRADA DE DATOS
+
+radio = float(input("Escriba el 1er número")) 
+
+#DECLARAR CONSTANTES
+
+pi = 3.1416
+
+#PROCESO 
+
+área = pi* (radio**2)
+perímetro = 2*pi*radio
+
+
+#SALIDA DE DATOS 
+
+print(F"El área es ={round(área, 2)}")
+print(F"El perímetro es = {round (perímetro, 2)}")
