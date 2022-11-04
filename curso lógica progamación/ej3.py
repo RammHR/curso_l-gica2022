22f3e156bc16add4e84458dd6e377f3b16f6bf47
RammHR <116772563+RammHR@users.noreply.github.com>
2022-11-03 19:22:58 -0600

    Add files via upload

---
+++ curso lógica progamación/ej3.py
@@ -0,0 +1,12 @@
+#ENTRADA DE DATOS 
+
+año_nacimiento = float(input("Escriba el año de nacimiento"))
+año_actual = 2022
+
+#PROCESO
+
+resta = año_nacimiento - año_actual
+
+#SALIDA DE DATOS 
+ 
+print(F"La edad es ={resta}")
