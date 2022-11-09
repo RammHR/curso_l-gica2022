5a49ac4376a43be4da72aa2993bec085260d350c
RammHR <116772563+RammHR@users.noreply.github.com>
2022-11-08 18:17:36 -0600

    Add files via upload

--- curso lógica progamación/ej1.py
+++ curso lógica progamación/ej1.py
@@ -15,16 +15,17 @@ promedio = (calificación_1 + calificación_2 + calificación_3) / 3
 
 
 
+print("Tu promedio =", round(promedio , 1))
 
 #SALIDA DE DATOS 
 if(promedio > 6 and promedio <= 10):
-    print("Tu promedio es =", round(promedio , 1))
+ 
     print("APROBADO")
 elif(promedio == 6):
-    print("Tu promedio =", round(promedio , 1))
+
     print("APROBADO DE PANSASO")
 elif(promedio >= 0 and promedio < 6):
-    print("Tu promedio =", round(promedio , 1))
+
     print("REPROBADO")
 else:
     print("PROMEDIO INVALIDO")
