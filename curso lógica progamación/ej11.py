7170f4af56a3c3000f93f6a169e1f93497ec6617
RammHR <116772563+RammHR@users.noreply.github.com>
2022-11-11 17:11:10 -0600

    Add files via upload

---
+++ curso lógica progamación/ej11.py
@@ -0,0 +1,19 @@
+
+
+arreglo = [2, 4, 6, 8, 10, 3, 5, 7, 0, 9]
+
+#MODIFICAR ELEMENTO
+
+arreglo[2] = 22
+
+#AGREGAR ELEMENTO
+
+arreglo.append(18)
+
+
+#ORDENAR FORMA ASC
+
+arreglo.sort()
+
+print("Arreglo", arreglo)
+
